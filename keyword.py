68b8055876552da666b0b3524c018a1d053e906a
basil <basilsunnyalukka@gmail.com>
2020-10-20 17:03:25 +0530

    check keyword

---
+++ keyword.py
@@ -0,0 +1,7 @@
+#checking the keyword
+n = input()
+if (n in ['break', 'case', 'continue', 'default', 'defer', 'else', 'for', 'func', 'goto', 'if', 'map', 'range', 'return', 'struct', 'type', 'var']):
+  		print(n + " is a keyword")
+else:
+  		print(n + " is not a keyword")
+  
