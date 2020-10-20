7facbd75bf08e460463fdca819d4d09a195fe1f3
basil <basilsunnyalukka@gmail.com>
2020-10-20 16:37:57 +0530

    hextodec

---
+++ hextodecimal.cpp
@@ -0,0 +1,32 @@
+// CONVERT HEXADECIMAL TO DECIMAL
+//Given a maximum of four digit to the base 17(10 -> A, 11 -> B, 12 -> C, 16 -> G) as input, output its decimal value.
+#include <iostream>
+#include <math.h>
+#include <string.h>
+using namespace std;
+int main(){
+  char hex[17];
+  long long decimal;
+  int i = 0, val, len;
+  decimal = 0;
+  cin>> hex;
+  len = strlen(hex);
+  len--;
+
+  for(i = 0;hex[i]!='\0';i++)
+  {
+    if(hex[i]>='0'&& hex[i]<='9'){
+      val = hex[i] - 48;
+    }
+    else if(hex[i]>='a'&& hex[i]<='g'){
+      val = hex[i] - 97 + 10;
+    }
+    else if(hex[i]>='A'&& hex[i]<='G'){
+      val = hex[i] - 65 + 10;
+    }
+    decimal = decimal + val * pow(17,len);
+    len--;
+  }
+  cout<< decimal;
+  return 0;
+}
